fe323958abd2693d2a597d2ac07ce38f155fb4a0
Nawin Raj Kumar <nrknawinrajkumar888@gmail.com>
2024-07-18 14:44:01 +0530

    Updated main.py - YOLOv8 Pose Estimation

--- scripts/main.py
+++ scripts/main.py
@@ -20,14 +20,25 @@ while True:
     results = model.predict(resized_frame)
 
     for result in results:
-        keypoints = result.keypoints.xyn[0]
+        keypoints = result.keypoints.xy[0]
 
+        # Get the first and second keypoints
         first_point = keypoints[0].cpu().numpy()
         second_point = keypoints[1].cpu().numpy()
 
-        cv2.line(resized_frame, (int(first_point[0]), int(first_point[1])),
-                  (int(second_point[0]), int(second_point[1])), (0, 255, 0), 2)
+        # Draw circles around the keypoints
+        cv2.circle(resized_frame, 
+                   (int(first_point[0]), int(first_point[1])), 
+                   5, (0, 255, 0), -1)
+        
+        cv2.circle(resized_frame, 
+                   (int(second_point[0]), int(second_point[1])),
+                    5, (0, 255, 0), -1)
 
+        # Draw a line between the keypoints
+        cv2.line(resized_frame, (int(first_point[0]), int(first_point[1])),
+                  (int(second_point[0]), int(second_point[1])), (255, 0, 0), 2)
+        
     # Display the frame
     cv2.imshow("frame", resized_frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
